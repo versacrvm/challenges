6a4d7dfbc7c041702a2a391ca023d386c55dc6ec
FABIAN <fabian.riess@live.de>
2024-04-10 11:56:05 +0200

    add changes

---
+++ script.js
@@ -0,0 +1,7 @@
+let mealCost = 200;
+let tipPercentage = 0.1;
+let tipAmount = mealCost * tipPercentage;
+let totalCount = tipAmount + mealCost;
+let text = "The total cost of your meal is " + totalCount + " euros.";
+
+console.log(text);
