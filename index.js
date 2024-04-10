4fc9b9ec053363e4cd7adbd0a9286c1f468dfa96
FABIAN <fabian.riess@live.de>
2024-04-10 13:56:14 +0200

    add changes to js file

---
+++ index.js
@@ -0,0 +1,7 @@
+let radiusCircle = 5;
+let circleArea = Math.PI * Math.pow(radiusCircle, 2);
+let circumference = 2 * Math.PI * radiusCircle;
+
+console.log("For a circle with a radius of 5 units:");
+console.log("The area is: ", circleArea);
+console.log("The circumference is: ", circumference);
