9ecbd70d9627153267b90fbb0c83af8d885f94de
FABIAN <fabian.riess@live.de>
2024-04-16 12:32:13 +0200

    add changes to multiple-inputs

--- js-inputs-strings/js-inputs-and-strings_multiple-inputs/js/index.js
+++ js-inputs-strings/js-inputs-and-strings_multiple-inputs/js/index.js
@@ -14,3 +14,21 @@ Implement the following functionality:
 const firstInput = document.querySelector("[data-js=first-input]");
 const secondInput = document.querySelector("[data-js=second-input]");
 const button = document.querySelector("[data-js=button]");
+const button2 = document.querySelector("[data-js=button2]");
+const button3 = document.querySelector("[data-js=button3]");
+
+button.addEventListener("click", () => {
+  const copyfirstInput = firstInput.value;
+  secondInput.value = copyfirstInput;
+});
+
+button2.addEventListener("click", () => {
+  const copyfirstInput = firstInput.value.toUpperCase();
+  secondInput.value = copyfirstInput;
+});
+
+button3.addEventListener("click", () => {
+  const switchFirstInput = firstInput.value;
+  firstInput.value = secondInput.value;
+  secondInput.value = switchFirstInput;
+});
