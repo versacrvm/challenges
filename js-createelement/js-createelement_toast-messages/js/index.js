e0d6fac7a14cf249ce52393cce70c082d56dc04e
FABIAN <fabian.riess@live.de>
2024-04-17 12:03:43 +0200

    add changes to toast messages

--- js-createelement/js-createelement_toast-messages/js/index.js
+++ js-createelement/js-createelement_toast-messages/js/index.js
@@ -5,9 +5,20 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+  // Exercise 1: Append a new entry to the toast messages container
+  //console.log("I clicked the button");
+  const newEntry = document.createElement("li");
+  newEntry.textContent = "Here is another message";
+  newEntry.classList.add(["toast-container__message"]);
+  const orderedList = document.querySelector('[data-js="toast-container"]');
+  orderedList.append(newEntry);
+  //console.log(newEntry);
 });
 
 clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+  // Exercise 2: Clear the stack of toast messages
+  const messageContainer = document.querySelector(
+    '[data-js="toast-container"]'
+  );
+  messageContainer.innerHTML = "";
 });
