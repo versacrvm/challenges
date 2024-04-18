8954736aaabcd5fadcb08a9b98cd835d33ec7d5f
FABIAN <fabian.riess@live.de>
2024-04-18 10:29:43 +0200

    add functions to exercise

--- JS-callback-functions/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
+++ JS-callback-functions/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -1,8 +1,15 @@
 console.clear();
-
 function handleUserLogin(onSuccess) {
   // You might wonder why this function is called `onSuccess`. This will become clear by the fourth challenge! 🤞
-  onSuccess();
 }
 
 // Call handleUserLogin below!
+
+function showWelcomeMessage() {
+  console.log("Welcome! You are logged in now");
+}
+
+handleUserLogin(showWelcomeMessage);
+handleUserLogin(function () {
+  console.log("Welcome! You are logged in now");
+});
