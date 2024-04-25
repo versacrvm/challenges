c7d5211c2d055c3c32bab6333ad8770a53c2f5b4
FABIAN <fabian.riess@live.de>
2024-04-25 16:53:56 +0200

    add changes

--- js-array-methods-2/js-array-methods-2_fun-with-flags/index.js
+++ js-array-methods-2/js-array-methods-2_fun-with-flags/index.js
@@ -9,10 +9,12 @@ queryInput.addEventListener("input", (event) => {
 
   const searchString = event.target.value;
 
-  const foundCountry = null;
+  const foundCountries = countries.filter((country) => {
+    return country.name.startsWith(searchString);
+  });
 
-  if (foundCountry) {
-    const countryElement = Country(foundCountry);
+  foundCountries.forEach((country) => {
+    const countryElement = Country(country);
     container.append(countryElement);
-  }
+  });
 });
