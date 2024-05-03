d8d9bd0bf4822aeb50bf107542ceb7b1329b3ec6
FABIAN <fabian.riess@live.de>
2024-05-03 16:08:31 +0200

    add function to app.jsx

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,16 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return (
+    <article className="article">
+      <h2 className="article__title">I'm a title</h2>
+      <label htmlFor="connect">i'm a label</label>
+      <input id="connect" type="text" />
+      <a
+        textContent="something to know"
+        className="article__link"
+        href="https://stackoverflow.com/questions/12894169/what-is-the-html-for-attribute-in-label"
+      ></a>
+    </article>
+  );
 }
