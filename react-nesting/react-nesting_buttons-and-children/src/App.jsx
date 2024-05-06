ee01ff24c4e3bc9b212e06f6def6989b7985aa7b
FABIAN <fabian.riess@live.de>
2024-05-06 16:05:21 +0200

    add children prop

--- react-nesting/react-nesting_buttons-and-children/src/App.jsx
+++ react-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -3,18 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>hello</Button>
+      <Button>hi</Button>
+      <Button>hey</Button>
+      <Button>hy</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
