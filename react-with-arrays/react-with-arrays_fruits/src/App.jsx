3b7d72d2a2c0fb9bfd7b5e94b7203b65d969ee9f
FABIAN <fabian.riess@live.de>
2024-05-10 10:51:41 +0200

    add .map and key

--- react-with-arrays/react-with-arrays_fruits/src/App.jsx
+++ react-with-arrays/react-with-arrays_fruits/src/App.jsx
@@ -1,13 +1,38 @@
 import Card from "./components/Card";
 import "./App.css";
 
+const fruits = [
+  {
+    id: 1337,
+    name: "🍌 Banana",
+    color: "yellow",
+  },
+  {
+    id: 1338,
+    name: "🍎 Apple",
+    color: "red",
+  },
+  {
+    id: 1339,
+    name: "🍋 Lemon",
+    color: "yellow",
+  },
+  {
+    id: 1340,
+    name: "🍊 Orange",
+    color: "orange",
+  },
+  {
+    id: 1341,
+    name: "🍒 Cherry",
+    color: "red",
+  },
+];
 
 export default function App() {
-  const fruits = [];
+  const fruitList = fruits.map((fruit) => (
+    <Card name={fruit.name} key={fruit.id} />
+  ));
 
-  return (
-    <div className="app">
-      <Card name="🍌 banana" />
-    </div>
-  );
+  return <div className="app">{fruitList}</div>;
 }
