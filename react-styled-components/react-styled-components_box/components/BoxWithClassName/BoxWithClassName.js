b4303a1e7057965c446231965b755c9c5f52df51
Fabian <153866165+versacrvm@users.noreply.github.com>
2024-05-21 15:52:55 +0200

    Delete react-styled-components/react-styled-components_box directory

--- react-styled-components/react-styled-components_box/components/BoxWithClassName/BoxWithClassName.js
+++
@@ -1,9 +0,0 @@
-export default function BoxWithClassName({ isBlack }) {
-  return (
-    <div
-      className={`box-with-classname${
-        isBlack ? " box-with-classname--black" : ""
-      }`}
-    ></div>
-  );
-}
