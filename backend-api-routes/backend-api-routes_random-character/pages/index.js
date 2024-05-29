21b47868c254fdc4bffb944a5fc88a10db8e3f36
FABIAN <fabian.riess@live.de>
2024-05-29 15:59:33 +0200

    added api and frontend

--- backend-api-routes/backend-api-routes_random-character/pages/index.js
+++ backend-api-routes/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,20 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, error } = useSWR("/api/random-character", fetcher);
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <>
+      <h1>Here's a random Character:</h1>
+      <p>First Name: {data.firstName}</p>
+      <p>Last Name: {data.lastName}</p>
+      <p>Twitter Name: {data.twitterName}</p>
+      <p>Geohash: {data.geoHash}</p>
+      <p>Age: {data.age}</p>
+    </>
+  );
 }
