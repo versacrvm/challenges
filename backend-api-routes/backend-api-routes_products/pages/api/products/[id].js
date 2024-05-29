8b930dbdf6936c09427cc5a811aafb0fa9586e86
FABIAN <fabian.riess@live.de>
2024-05-29 16:34:40 +0200

    add api and id

---
+++ backend-api-routes/backend-api-routes_products/pages/api/products/[id].js
@@ -0,0 +1,12 @@
+import { getProductById } from "../../../services/productServices.js";
+
+export default function handler(request, response) {
+  const { id } = request.query;
+  const product = getProductById(id);
+
+  if (!product) {
+    return response.status(404).json({ status: "Not found" });
+  }
+
+  response.status(200).json(product);
+}
