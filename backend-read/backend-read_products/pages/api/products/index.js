bfd84f4f241b8f01df9d21774cb44dc2d54923fe
FABIAN <fabian.riess@live.de>
2024-05-31 15:49:58 +0200

    added backend connection

--- backend-read/backend-read_products/pages/api/products/index.js
+++ backend-read/backend-read_products/pages/api/products/index.js
@@ -1,5 +1,13 @@
-import { products } from "../../../lib/products";
+import dbConnect from "../../../db/dbConnect.js";
+import Product from "../../../db/models/product.js";
 
-export default function handler(request, response) {
-  return response.status(200).json(products);
+export default async function handler(request, response) {
+  await dbConnect();
+  if (request.method === "GET") {
+    const products = await Product.find();
+    if (!products) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+    response.status(200).json(products);
+  }
 }
