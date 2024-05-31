b7c21d2c347ef4f6809fd55cdabd11ab4664c3ef
FABIAN <fabian.riess@live.de>
2024-05-31 16:01:25 +0200

    add individual product route

--- backend-read/backend-read_products/pages/api/products/[id].js
+++ backend-read/backend-read_products/pages/api/products/[id].js
@@ -1,13 +1,12 @@
-import { products } from "../../../lib/products";
+import dbConnect from "../../../db/dbConnect.js";
+import Product from "../../../db/models/product.js";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
   const { id } = request.query;
+  await dbConnect();
 
-  const product = products.find((product) => product.id === id);
-
-  if (!product) {
-    return response.status(404).json({ status: "Not Found" });
+  if (request.method === "GET") {
+    const product = await Product.findById(id);
+    return response.status(200).json(product);
   }
-
-  response.status(200).json(product);
 }
