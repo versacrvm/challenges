608dd4e994a7283a62b99865b57c40c7a0c10806
FABIAN <fabian.riess@live.de>
2024-06-03 11:13:26 +0200

    add POST route

--- Backend-Create/backend-create_products/pages/api/products/index.js
+++ Backend-Create/backend-create_products/pages/api/products/index.js
@@ -8,4 +8,14 @@ export default async function handler(request, response) {
     const products = await Product.find();
     return response.status(200).json(products);
   }
+  if (request.method === "POST") {
+    try {
+      const productData = request.body;
+      await Product.create(productData);
+      response.status(201).json({ status: "Product created." });
+    } catch (e) {
+      console.log("Error in POST in /", e);
+      response.status(400).json({ error: error.message });
+    }
+  }
 }
