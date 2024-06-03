7067c5feb369dcd356d2c793deac73204d182564
FABIAN <fabian.riess@live.de>
2024-06-03 16:11:22 +0200

    add PUT functionality

--- backend-update-delete/backend-update-and-delete_products/components/Product/index.js
+++ backend-update-delete/backend-update-and-delete_products/components/Product/index.js
@@ -3,12 +3,15 @@ import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import { StyledLink } from "../Link/Link.styled";
 import Comments from "../Comments";
+import { useState } from "react";
+import ProductForm from "../ProductForm";
 
 export default function Product() {
+  const [isEditMode, setIsEditMode] = useState(false);
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -18,6 +21,25 @@ export default function Product() {
     return;
   }
 
+  async function handleEditProduct(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  }
+
   return (
     <ProductCard>
       <h2>{data.name}</h2>
@@ -25,6 +47,21 @@ export default function Product() {
       <p>
         Price: {data.price} {data.currency}
       </p>
+      <button
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        ?proper text?
+      </button>
+      {isEditMode && (
+        <ProductForm
+          onSubmit={handleEditProduct}
+          value={data.product}
+          isEditMode={true}
+        />
+      )}
       {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
       <StyledLink href="/">Back to all</StyledLink>
     </ProductCard>
