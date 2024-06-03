db5a260ed902fbf2825de9855b70c803b0480dd6
FABIAN <fabian.riess@live.de>
2024-06-03 11:45:51 +0200

    debugging

--- Backend-Create/backend-create_products/pages/api/products/index.js
+++ Backend-Create/backend-create_products/pages/api/products/index.js
@@ -3,7 +3,6 @@ import Product from "../../../db/models/Product";
 
 export default async function handler(request, response) {
   await dbConnect();
-
   if (request.method === "GET") {
     const products = await Product.find();
     return response.status(200).json(products);
